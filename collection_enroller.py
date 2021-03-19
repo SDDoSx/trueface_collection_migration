e18ac5570256f1bc80fa57f21a8bc88906ca0582
SDDoSX <moudni3@gmail.com>
2021-03-19 15:07:54 -0600

    Added method and comments

--- collection_enroller.py
+++ collection_enroller.py
@@ -1,19 +1,35 @@
 import requests
 from databse_retriever import readBLOB
 
+"""Make sure that visionbox is running"""
+
+size_db = 5 #set the size of your database (number of identities you'll need to enroll)
+
+
 url = "http://0.0.0.0:8080/v1/enroll"
 
-payload={'namespace': 'client123',
-'collection_id': 'office_developers',
-'label': 'Manuel'}
-files=[
-  ('image',('manuel.jpeg',open('/home/mark/Trueface/visionbox/manuel.jpeg','rb'),'image/jpeg'))
-]
-headers = {
-  'Accept': '*/*'
-}
 
-response = requests.request("POST", url, headers=headers, data=payload, files=files)
+for x in range(size_db - 1):
+    #This function will take the ID of the identity and store the picture in a chosen location named by it's ID.
+    #To make it simple I'm just assuming my identities are named 0, 1, 2, 3, 4 (To make the recurssion easier)
+    #Note that the ID can be the user name or an UUID. The loop integration will vary on those cases
+    readBLOB(x,"/chosen/folder/location/to/store/the/picture"+x)
 
-print(response.text)
+#Now all the pictures are named and stored in a desired folder
+
+for x in range(size_db - 1):
+    payload = {'namespace': 'Your_name_space',
+               'collection_id': 'Your_collection_ID',
+               'label': x}
+    files = [
+      ('image', (x + '.jpeg', open('/chosen/folder/location/to/store/the/picture/' + x + '.jpeg', 'rb'), 'image/jpeg'))
+    ]
+    headers = {
+      'Accept': '*/*'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    #This is to make sure that the identity was enrolled succesfully
+    print(response.text)
+
 
